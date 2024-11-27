3e592d24c21e5721d865a713c7f1f927a9bbe384
’zuolingfeng‘ <907619699@qq.com>
2024-11-27 09:28:01 +0800

    部署

---
+++ vue-project/dmy_dist/static/js/AboutView-DRscPhhe.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-Cari7bGh.js";import{f as e,g as s,o as r}from"./@vue-B43FY_TO.js";import"./pinia-DxndeY0C.js";import"./vue-router-CEMnpouV.js";const a={},n={class:"about"};function c(i,o){return r(),e("div",n,o[0]||(o[0]=[s("h1",null,"This is an about page",-1)]))}const l=t(a,[["render",c]]);export{l as default};
