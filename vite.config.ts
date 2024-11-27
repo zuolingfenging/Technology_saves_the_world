4af3c04af86e38c13aa5e3af53fff43928b89302
’zuolingfeng‘ <907619699@qq.com>
2024-11-27 10:02:22 +0800

    部署完成

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
- 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
- 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: '/',
+  base: './',
+  plugins: [vue(),],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
